0b948c34560df9ac5a0ca69aaa4f9c82b4f23e7a
jeremie <bouenieazongomajeremiejose@gmail.com>
2026-02-14 22:00:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,11 +2,14 @@ self.addEventListener("install", event => {
   event.waitUntil(
     caches.open("puzzle-cache").then(cache => {
       return cache.addAll([
-        "/",
         "index.html",
         "puzzle.html",
         "script.js",
-        "puzzle.js"
+        "puzzle.js",
+        "style.css",
+        "images/photo1.jpg",
+        "images/photo1,1.jpg",
+        "images/photo1,2.jpg"
       ]);
     })
   );
