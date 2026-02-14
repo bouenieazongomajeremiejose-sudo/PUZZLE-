14f07b9c0840033d6aa28bc0253e6809a9096964
jeremie <bouenieazongomajeremiejose@gmail.com>
2026-02-14 23:04:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -53,17 +53,46 @@ const urlsToCache = [
  "image/vidéO 4.mp4"
 ];
 
+
+
+
+// INSTALLATION
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// ACTIVATE (supprime anciens cache)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// FETCH intelligent
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
 
